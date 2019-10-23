9dceb3d576b16ef642d8236c2e1dec035e984d45
BuildTools <unconfigured@null.spigotmc.org>
2019-10-22 23:17:34 -0300

    Update

--- scripts/crearTabla.sql
+++ scripts/crearTabla.sql
@@ -7,3 +7,14 @@ CREATE TABLE IF NOT EXISTS test.estudiantes (
 	fechaNacimiento DATE NOT NULL,
 	CONSTRAINT estudiantes_PK PRIMARY KEY (id)
 );
+insert into estudiantes values
+("","Juan","Pollo","2002-12-25"),
+("","Roberto","Asado","200-3-18"),
+("","Diana","cazuela de mariscos","2002-9-19"),
+("","Renata","Papa rellena","1998-5-6"),
+("","Carolina","risotto","1971-3-26"),
+("","Azul","arroz","1400-5-3"),
+("","Carla","milanesa","1988-4-15"),
+("","Lucia","sopa","1966-2-37"),
+("","Sabrina","tarta","1999-1-31"),
+("","Joaquin","brocoli","2006-10-26");
